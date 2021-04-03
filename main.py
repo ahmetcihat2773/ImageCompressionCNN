f24a2b623b155a0e2b185020d4fac572c86f0eec
Ahmet Cihat Bozkurt <ahmetcihat2773@gmail.com>
2021-04-03 10:08:14 +0200

    JPEG is ready continue with JPEG2000

--- main.py
+++ main.py
@@ -56,51 +56,72 @@ class ImageCompression():
 
 
         
-    def compressImage(self,up_lim,down_lim,compression_type,quality,same_file_size=False,same_ssmi = False):
+    def compressImage_JPEG(self,up_lim,down_lim,quality,same_file_size=False,same_ssmi = False):
         """
         Compress file in jpeg format with given file_size
         """
+        compression_type = "JPEG"
         compressed_num = 0
         try_num = 0
-        folderpath = "4288-2848/" 
-        outfolder = "4288-2848_JPEG/"
         low_quality = 1
         high_quality = 100
-        if not os.path.exists(outfolder):
-            os.makedirs(outfolder)
-        for filename in os.listdir(folderpath):
-            try:
-                if compression_type == "JPEG" and same_file_size and "tiff" in filename:
-                    filepath = folderpath + filename
-                    picture = Image.open(filepath)
-                    save_name = "Compressed_" + str(compressed_num) + "_"+ str(quality) + ".jpg" 
-                    save_path = outfolder + save_name
-                    picture.save(save_path,compression_type,optimize = True,quality = quality)
-                    compressed_size = os.stat(save_path).st_size
-                    while not (compressed_size> down_lim and compressed_size < up_lim):
-                        try_num = try_num + 1
-                        if try_num > 150:
-                            os.remove(save_path)
-                            break
-                        if compressed_size < down_lim:
-                            # If size is lower then up_lim increase the quality to make it bigger.
-                            low_quality = low_quality + 1
-                            picture.save(save_path,compression_type,optimize = False,quality = low_quality)
-                        else:
-                            if high_quality <=1:
-                                high_quality = 20
-                            high_quality = high_quality - 1
-                            picture.save(save_path,compression_type,optimize = False,quality = high_quality)
+        folderpath = "4288-2848/" 
+        if same_file_size:
+            outfolder = "4288-2848_JPEG_samesize/"
+            if not os.path.exists(outfolder):
+                os.makedirs(outfolder)
+            for filename in os.listdir(folderpath):
+                try:
+                    if "tiff" in filename:
+                        filepath = folderpath + filename
+                        picture = Image.open(filepath)
+                        save_name = "Compressed_" + str(compressed_num) + "_"+ str(quality) + ".jpg" 
+                        save_path = outfolder + save_name
+                        picture.save(save_path,compression_type,optimize = True,quality = quality)
                         compressed_size = os.stat(save_path).st_size
-                        print(compressed_size)
-                compressed_num = compressed_num + 1 
-                print(save_name)
-            except:
-                print("ERROR")
-                continue
-            low_quality = 1
-            high_quality = 50    
-            try_num = 0        
-    
+                        while not (compressed_size> down_lim and compressed_size < up_lim):
+                            try_num = try_num + 1
+                            if try_num > 150:
+                                os.remove(save_path)
+                                break
+                            if compressed_size < down_lim:
+                                # If size is lower then up_lim increase the quality to make it bigger.
+                                low_quality = low_quality + 1
+                                picture.save(save_path,compression_type,optimize = False,quality = low_quality)
+                            else:
+                                if high_quality <=1:
+                                    high_quality = 20
+                                high_quality = high_quality - 1
+                                picture.save(save_path,compression_type,optimize = False,quality = high_quality)
+                            compressed_size = os.stat(save_path).st_size
+                            print(compressed_size)
+                    compressed_num = compressed_num + 1 
+                    print(save_name)
+                except:
+                    "In case of error continue with the next file."
+                    continue
+                low_quality = 1
+                high_quality = 50    
+                try_num = 0        
+        if same_ssmi:
+            outfolder = "4288-2848_JPEG_samessmi/"
+            if not os.path.exists(outfolder):
+                os.makedirs(outfolder)
+            for filename in os.listdir(folderpath):
+                try:
+                    if "tiff" in filename:
+                        filepath = folderpath + filename
+                        picture = Image.open(filepath)
+                        save_name = "Compressed_" + str(compressed_num) + "_"+ str(quality) + ".jpg" 
+                        save_path = outfolder + save_name
+                        picture.save(save_path,compression_type,optimize = True,quality = quality)
+                        compressed_num = compressed_num + 1
+                except:
+                    continue
+    def compressImage_JPEG2(self):
+        print("STARTED") 
+                       
+
+
 compressionClass = ImageCompression()
-compressionClass.compressImage(up_lim = 250000,down_lim = 240000,quality= 10,compression_type= "JPEG",same_file_size=True)
+compressionClass.compressImage_JPEG(up_lim = 250000,down_lim = 240000,quality= 20,same_file_size=False,same_ssmi = True)
